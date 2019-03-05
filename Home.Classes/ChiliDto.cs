66df4cd031b1fc7be5b98ddcd7e3c9e76b647e21
Marcel Benders <git@b-velop.com>
2019-03-05 22:52:43 +0100

    (dev) added ChiliDto

---
+++ Home.Classes/ChiliDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace com.b_velop.Home.Classes
+{
+    public class ChiliDto
+    {
+        public DateTimeOffset Created { get; set; }
+        public double Ground { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public bool Heater { get; set; }
+        public bool Light { get; set; }
+    }
+}
